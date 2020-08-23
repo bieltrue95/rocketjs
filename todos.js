6575ea030c4b01e06eb3a68676cffc55a9a1a26f
Gabriel Santos <devgtrue@gmail.com>
2020-08-23 20:42:17 -0300

    Criando Todos

--- todos.js
+++ todos.js
@@ -8,7 +8,8 @@ var todos = [
     'Acessar comunidade da Rocketseat'
 ];
 
-function renderTodos()   {
+function renderTodos() {
+    listElement.innerHTML = '';
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
@@ -19,3 +20,13 @@ function renderTodos()   {
 }
 
 renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
