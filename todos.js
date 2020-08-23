860fd04e182fe4182bb4a80d6a1c887bc5c17317
Gabriel Santos <devgtrue@gmail.com>
2020-08-23 20:25:49 -0300

    Renderizando Todos

--- todos.js
+++ todos.js
@@ -1,9 +1,21 @@
-var listElement = document. querySelector('#app ul');
-var imputElement = document.querySelector('#app input');
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
 var todos = [
-'Fazer café',   
-'Estudar JavaScript',
-'Acessar comunidade da Rocketseat'
+    'Fazer café',
+    'Estudar JavaScript',
+    'Acessar comunidade da Rocketseat'
 ];
+
+function renderTodos()   {
+    for (todo of todos) {
+        var todoElement = document.createElement('li');
+        var todoText = document.createTextNode(todo);
+
+        todoElement.appendChild(todoText);
+        listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
