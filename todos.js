41f1590ba7cc5493a0b1bca4a5f175ce52559bff
Gabriel Santos <devgtrue@gmail.com>
2020-08-23 22:38:07 -0300

    excluindo Todos

--- todos.js
+++ todos.js
@@ -10,12 +10,34 @@ var todos = [
 
 function renderTodos() {
     listElement.innerHTML = '';
+
+
     for (todo of todos) {
         var todoElement = document.createElement('li');
         var todoText = document.createTextNode(todo);
 
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+
+        var linkText = document.createTextNode('Excluir')
+
+        linkElement.appendChild(linkText);
+
+        var pos = todos.indexOf(todo);
+        var pos = todos.indexOf(todo);
+        linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+        var linkText = document.createTextNode('Excluir')
+
         todoElement.appendChild(todoText);
+        todoElement.appendChild(linkElement);
+
         listElement.appendChild(todoElement);
+
+
+
+
+
     }
 }
 
@@ -30,3 +52,8 @@ function addTodo() {
 }
 
 buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
+    renderTodos();
+}
