aafb0ae5c85e3a301423ab11a1c933cd8616daea
Gabriel Santos <devgtrue@gmail.com>
2020-08-23 22:58:50 -0300

    Salvando no storage

--- todos.js
+++ todos.js
@@ -2,15 +2,11 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar JavaScript',
-    'Acessar comunidade da Rocketseat'
-];
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
 
 function renderTodos() {
     listElement.innerHTML = '';
-
 
     for (todo of todos) {
         var todoElement = document.createElement('li');
@@ -33,16 +29,10 @@ function renderTodos() {
         todoElement.appendChild(linkElement);
 
         listElement.appendChild(todoElement);
-
-
-
-
-
     }
 }
 
 renderTodos();
-
 function addTodo() {
     var todoText = inputElement.value;
 
@@ -50,10 +40,15 @@ function addTodo() {
     inputElement.value = '';
     renderTodos();
 }
-
 buttonElement.onclick = addTodo;
 
 function deleteTodo(pos) {
     todos.splice(pos, 1);
     renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
